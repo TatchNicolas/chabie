a2468d339f68eef2085303b0eb4751ef54342e0b
TatchNicolas <TatchNicolas@users.noreply.github.com>
2018-10-06 19:53:36 +0900

    WIP

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chabie',
-    version='0.1',
+    version='0.2',
     description='Comparison utility',
     author='TatchNicolas',
     author_email='TatchNicolas@users.noreply.github.com',
