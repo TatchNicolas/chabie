38bc2cd288be364f718f85b0c26e39f52fde35f2
TatchNicolas <TatchNicolas@users.noreply.github.com>
2018-10-06 18:17:38 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(
+    name='chabie',
+    version='0.1',
+    description='Comparison utility',
+    author='TatchNicolas',
+    author_email='TatchNicolas@users.noreply.github.com',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
