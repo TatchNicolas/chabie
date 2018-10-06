44be48f5d0771b12e2f7fd229b38af744de358a6
TatchNicolas <TatchNicolas@users.noreply.github.com>
2018-10-06 18:32:04 +0900

    rename vars

--- src/chabie.py
+++ src/chabie.py
@@ -1,7 +1,7 @@
 from typing import Dict, List, Tuple
 
 
-def dictcomp(a: Dict, b: Dict, key: List[Tuple] = None) -> bool:
+def compare_dicts(a: Dict, b: Dict, key: List[Tuple] = None) -> bool:
 
     if type(a) != type(b):
         return False
@@ -9,14 +9,14 @@ def dictcomp(a: Dict, b: Dict, key: List[Tuple] = None) -> bool:
     if isinstance(a, dict):
         if not sorted(a.keys()) == sorted(b.keys()):
             return False
-        return all([dictcomp(a[key], b[key]) for key in a.keys()])
+        return all([compare_dicts(a[key], b[key]) for key in a.keys()])
 
     if isinstance(a, list):
         if len(a) != len(b):
             return False
         return all(
             [
-                dictcomp(a_element, b_element)
+                compare_dicts(a_element, b_element)
                 for a_element, b_element in zip(a, b)
             ]
         )
