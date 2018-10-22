11d85e3210e824323528f05fa5952bcfaa7ee84f
TatchNicolas <TatchNicolas@users.noreply.github.com>
2018-10-22 20:37:57 +0900

    Worked with dirty implementation

--- chabie/chabie.py
+++ chabie/chabie.py
@@ -1,7 +1,7 @@
-from typing import Dict, List, Tuple
+from typing import Dict
 
 
-def compare_dicts(a: Dict, b: Dict, key_list: List[Tuple] = None) -> bool:
+def compare_dicts(a: Dict, b: Dict, key_list: Dict) -> bool:
 
     if type(a) != type(b):
         return False
@@ -9,16 +9,32 @@ def compare_dicts(a: Dict, b: Dict, key_list: List[Tuple] = None) -> bool:
     if isinstance(a, dict):
         if not sorted(a.keys()) == sorted(b.keys()):
             return False
-        return all([compare_dicts(a[key], b[key]) for key in a.keys()])
+        return all(
+            [compare_dicts(a[key], b[key], key_list) for key in a.keys()]
+        )
 
     if isinstance(a, list):
         if len(a) != len(b):
             return False
         return all(
             [
-                compare_dicts(a_element, b_element)
+                compare_dicts(a_element, b_element, key_list)
                 for a_element, b_element in zip(a, b)
             ]
         )
 
+    print(key_list)
+    if type(a) in key_list:
+        print('Look!')
+        return key_list[type(a)](a, b)
+        # a.__eq__ = key_list[type(a)]
     return a == b
+
+
+print(
+    compare_dicts(
+        {'hoge': 'hoge'},
+        {'hoge': 'fuga'},
+        {type(''): lambda a, b: True}
+    )
+)
