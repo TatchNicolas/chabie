3c08bb501a15cf592aae4a34bc907bcb55dc28a4
TatchNicolas <TatchNicolas@users.noreply.github.com>
2018-12-02 11:55:35 +0900

    Test long description

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+long_description = '''
+Recursively compare dicts.
+Designed for Web API test.
+'''
+
 
 setup(
     name='chabie',
-    version='0.3.0',
+    version='0.3.3',
     description='Comparison utility',
+    long_description=long_description,
     author='TatchNicolas',
     author_email='TatchNicolas@users.noreply.github.com',
     license='MIT',
@@ -13,5 +19,6 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
+    url='https://github.com/TatchNicolas/chabie'
 )
